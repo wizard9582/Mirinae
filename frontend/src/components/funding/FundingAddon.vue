84e093b948d6a97d0224c9c18ffe1255f2c2ac76
wizard9582 <qkfskan82@naver.com>
2021-09-24 07:38:12 +0900

    [#S05P21A506-89] feat : 펀딩 상세페이지

---
+++ frontend/src/components/funding/FundingAddon.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="fixed opacity-80 bottom-0 left-0 w-full h-24 bg-gray-200">
+        <div class="mr-10 ml-auto mt-10 h-1/2 w-40 bg-gray-700 opacity-100 text-myfont">
+            펀딩 참여하기
+        </div>
+    </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+
+export default {
+    name: 'FundingAddon',
+    components: {
+    },
+
+    setup(){
+        const store = useStore()
+        const router = useRouter()
+        const state = reactive({})
+
+        return {state}
+    }
+};
+</script>
+
+<style scoped>
+</style>
